1f768e4fd0aa85c321a0419235807b52642be2eb
Mauricio Ouriques Toneto <matt_ouriques@hotmail.com>
2016-03-29 20:05:58 -0300

    criando classe model

---
+++ src/tb/controller/modelo/Pessoa.java
@@ -0,0 +1,12 @@
+package tb.controller.modelo;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.StringProperty;
+
+public class Pessoa {
+
+	 private final StringProperty Nome;
+	 private final IntegerProperty Telefone;
+	 private final IntegerProperty Id;
+	 
+}
