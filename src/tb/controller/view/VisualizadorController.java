0560ccb24d6a11258db47020b794626eddebcd4c
Mauricio Ouriques Toneto <matt_ouriques@hotmail.com>
2016-03-30 16:37:28 -0300

    Alterado metodo se selação para exibir mesagem de seleção nula

--- src/tb/controller/view/VisualizadorController.java
+++ src/tb/controller/view/VisualizadorController.java
@@ -1,4 +1,6 @@
 package tb.controller.view;
+import org.controlsfx.dialog.Dialogs;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
@@ -61,7 +63,15 @@ public class VisualizadorController {
 	    @FXML
 	    private void handleDeletePerson() {
 	        int selectedIndex = personTable.getSelectionModel().getSelectedIndex();
-	        personTable.getItems().remove(selectedIndex);
+	        if (selectedIndex >= 0) {
+	            personTable.getItems().remove(selectedIndex);
+	        } else {
+	            Dialogs.create()
+	                .title("Nenhuma seleção")
+	                .masthead("Nenhuma Pessoa Selecionada")
+	                .message("Por favor, selecione uma pessoa na tabela.")
+	                .showWarning();
+	        }
 	    }
 	    
 }
